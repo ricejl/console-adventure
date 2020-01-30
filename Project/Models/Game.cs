45395b7e87b37b0135688447f78a4e85d6585c22
Jenny <ricejl@gmail.com>
2020-01-30 11:43:28 -0700

    create rooms

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -10,7 +10,12 @@ namespace ConsoleAdventure.Project.Models
         //NOTE Make yo rooms here...
         public void Setup()
         {
-            throw new System.NotImplementedException();
+            // TODO create rooms
+            Room Tower = new Room("Tower", "You enter a lofty tower with open windows, the wind whipping the shudders to and frow. The moonlight gives the room an eerie glow.");
+            Room Den = new Room("Den", "Upon entering, you are struck with the smell of old books and musty leather, no doubt emanating from the shelves of books lining the west and south walls. In the center of the room is a large, mahogany wooden desk.");
+            Room Kitchen = new Room("Kitchen", "Dusty pans hang from the ceiling and a large selection of heavily used kitchen implements are scattered along the countertops. The room feels colder than the rest of the house. The metallic air permeates the entire space.");
+            Room DrawingRoom = new Room("Drawing Room", "Ancient, floral wallpaper curls from the walls at yellowed seams. Once heavily trafficked, the stillness is broken only by your footsteps across the creaking, wooden floorboards.");
+            Room Cellar = new Room("Cellar", "The space around you feels like the inside of a great stone beast, heavy with moisture and pulsating with life. You are unable to see anything.");
         }
     }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -9,5 +9,11 @@ namespace ConsoleAdventure.Project.Models
         public string Description { get; set; }
         public List<Item> Items { get; set; }
         public Dictionary<string, IRoom> Exits { get; set; }
+
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
     }
 }
